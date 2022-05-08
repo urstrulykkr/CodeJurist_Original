5051c033af466d7addeaadb8a59b6e90e2419d70
urstrulykkr <reddykaushik18@gmail.com>
2022-05-08 11:20:08 +0530

    done with UI

--- OnlineJudge/OnlineJudge/urls.py
+++ OnlineJudge/OnlineJudge/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "Online Judge Admin"
+admin.site.site_title = "Online Judge Admin Portal"
+admin.site.index_title = "Welcome to Online Judge"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('oj.urls')),
 ]
---
+++ OnlineJudge/oj/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('problem/<int:variable_id>/', views.problem, name='problem'),
+    path('problem/<int:variable_id>/submit', views.submit, name='submit'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register')
+]
