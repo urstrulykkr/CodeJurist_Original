6ca691686b1e04936dbdaabe7ca10aed60b2a241
urstrulykkr <reddykaushik18@gmail.com>
2022-05-28 21:56:05 +0530

    Added compiler functionality for code judging based on programming languages

--- OnlineJudge/OnlineJudge/settings.py
+++ OnlineJudge/OnlineJudge/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'oj.apps.OjConfig',
     'problem.apps.ProblemConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- OnlineJudge/OnlineJudge/urls.py
+++ OnlineJudge/OnlineJudge/urls.py
@@ -1,7 +1,6 @@
-"""OnlineJudge URL Configuration
-
+"""codecourt URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,11 +15,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Online Judge Admin"
-admin.site.site_title = "Online Judge Admin Portal"
-admin.site.index_title = "Welcome to Online Judge"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oj.urls')),
+    path('user/', include('user.urls')),
+    path('problem/', include('problem.urls')),
 ]
