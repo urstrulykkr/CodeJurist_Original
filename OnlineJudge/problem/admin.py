966e6d49ca009045cad27045238e05b36a558e5a
K. Kaushik Reddy <reddykaushik18@gmail.com>
2022-06-19 20:43:49 +0530

    Delete OnlineJudge directory

--- OnlineJudge/problem/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Problem, TestCase
-
-admin.site.register(Problem)
-admin.site.register(TestCase)
--- OnlineJudge/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register_n'),
-    path('login/', views.login, name='login_n'),
-    path('logout/', views.logout, name='logout_n'),
-    path('submissions/', views.submissions, name='submissions'),
-]
