3397254265a27a166fa9cc15965c432e9c093105
urstrulykkr <reddykaushik18@gmail.com>
2022-06-28 22:11:40 +0530

    Created home & problem apps

--- CodeJurist/CodeJurist/settings.py
+++ CodeJurist/CodeJurist/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'problem.apps.ProblemConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
